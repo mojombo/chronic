408fac995099a5dd767a78fd49fa86aea153f088
Lee Jarvis <lee@jarvis.co>
2011-05-25 08:54:33 +0100

    dont mess with the load path, instead use the -I flag for testing

--- lib/chronic.rb
+++ lib/chronic.rb
@@ -7,8 +7,6 @@
 #
 #=============================================================================
 
-$:.unshift File.dirname(__FILE__)     # For use/testing when no gem is installed
-
 require 'time'
 
 require 'chronic/chronic'
