d3ee1908d75770f294ba7058667d8b08ecfc4f9e
Lee Jarvis <lee@jarvis.co>
2011-05-27 17:55:01 +0100

    added Scalar::DAY_PORTIONS constant

--- lib/chronic/scalar.rb
+++ lib/chronic/scalar.rb
@@ -1,6 +1,8 @@
 module Chronic
 
   class Scalar < Tag #:nodoc:
+    DAY_PORTIONS = %w( am pm morning afternoon evening night )
+
     def self.scan(tokens)
       # for each token
       tokens.each_index do |i|
@@ -13,7 +15,7 @@ module Chronic
 
     def self.scan_for_scalars(token, post_token)
       if token.word =~ /^\d*$/
-        unless post_token && %w{am pm morning afternoon evening night}.include?(post_token)
+        unless post_token && DAY_PORTIONS.include?(post_token)
           return Scalar.new(token.word.to_i)
         end
       end
@@ -22,7 +24,7 @@ module Chronic
     def self.scan_for_days(token, post_token)
       if token.word =~ /^\d\d?$/
         toi = token.word.to_i
-        unless toi > 31 || toi < 1 || (post_token && %w{am pm morning afternoon evening night}.include?(post_token.word))
+        unless toi > 31 || toi < 1 || (post_token && DAY_PORTIONS.include?(post_token.word))
           return ScalarDay.new(toi)
         end
       end
@@ -31,7 +33,7 @@ module Chronic
     def self.scan_for_months(token, post_token)
       if token.word =~ /^\d\d?$/
         toi = token.word.to_i
-        unless toi > 12 || toi < 1 || (post_token && %w{am pm morning afternoon evening night}.include?(post_token.word))
+        unless toi > 12 || toi < 1 || (post_token && DAY_PORTIONS.include?(post_token.word))
           return ScalarMonth.new(toi)
         end
       end
@@ -39,7 +41,7 @@ module Chronic
 
     def self.scan_for_years(token, post_token)
       if token.word =~ /^([1-9]\d)?\d\d?$/
-        unless post_token && %w{am pm morning afternoon evening night}.include?(post_token.word)
+        unless post_token && DAY_PORTIONS.include?(post_token.word)
           return ScalarYear.new(token.word.to_i)
         end
       end
