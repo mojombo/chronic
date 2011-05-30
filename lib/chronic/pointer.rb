ef2497edc813dd0fe3316a89b72e0e0a5876ef28
Lee Jarvis <lee@jarvis.co>
2011-05-30 15:39:07 +0100

    use scan_for inside pointer scanner method

--- lib/chronic/pointer.rb
+++ lib/chronic/pointer.rb
@@ -10,13 +10,12 @@ module Chronic
     end
 
     def self.scan_for_all(token)
-      scanner = {/\bpast\b/ => :past,
-                 /\bfuture\b/ => :future,
-                 /\bin\b/ => :future}
-      scanner.keys.each do |scanner_item|
-        return self.new(scanner[scanner_item]) if scanner_item =~ token.word
-      end
-      return nil
+      scan_for token, self,
+      {
+        /\bpast\b/ => :past,
+        /\bfuture\b/ => :future,
+        /\bin\b/ => :future
+      }
     end
 
     def to_s
