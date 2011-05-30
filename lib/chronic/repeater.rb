6fa4830474b6d54044ee92a4741fee46610d69f2
Lee Jarvis <lee@jarvis.co>
2011-05-30 15:26:53 +0100

    use scan_for for repeater scanning methods
    also be explicit with Chronic module

--- lib/chronic/repeater.rb
+++ lib/chronic/repeater.rb
@@ -1,133 +1,127 @@
-class Chronic::Repeater < Chronic::Tag #:nodoc:
-  def self.scan(tokens)
-    # for each token
-    tokens.each_index do |i|
-      if t = self.scan_for_season_names(tokens[i]) then tokens[i].tag(t); next end
-      if t = self.scan_for_month_names(tokens[i]) then tokens[i].tag(t); next end
-      if t = self.scan_for_day_names(tokens[i]) then tokens[i].tag(t); next end
-      if t = self.scan_for_day_portions(tokens[i]) then tokens[i].tag(t); next end
-      if t = self.scan_for_times(tokens[i]) then tokens[i].tag(t); next end
-      if t = self.scan_for_units(tokens[i]) then tokens[i].tag(t); next end
-    end
-    tokens
-  end
-
-  def self.scan_for_season_names(token)
-    {
-      /^springs?$/ => :spring,
-      /^summers?$/ => :summer,
-      /^(autumn)|(fall)s?$/ => :autumn,
-      /^winters?$/ => :winter
-    }.each do |item, symbol|
-      return Chronic::RepeaterSeasonName.new(symbol) if item =~ token.word
-    end
-    return nil
-  end
-
-  def self.scan_for_month_names(token)
-    {
-      /^jan\.?(uary)?$/ => :january,
-      /^feb\.?(ruary)?$/ => :february,
-      /^mar\.?(ch)?$/ => :march,
-      /^apr\.?(il)?$/ => :april,
-      /^may$/ => :may,
-      /^jun\.?e?$/ => :june,
-      /^jul\.?y?$/ => :july,
-      /^aug\.?(ust)?$/ => :august,
-      /^sep\.?(t\.?|tember)?$/ => :september,
-      /^oct\.?(ober)?$/ => :october,
-      /^nov\.?(ember)?$/ => :november,
-      /^dec\.?(ember)?$/ => :december
-    }.each do |item, symbol|
-      return Chronic::RepeaterMonthName.new(symbol) if item =~ token.word
-    end
-    return nil
-  end
-
-  def self.scan_for_day_names(token)
-    {
-      /^m[ou]n(day)?$/ => :monday,
-      /^t(ue|eu|oo|u|)s(day)?$/ => :tuesday,
-      /^tue$/ => :tuesday,
-      /^we(dnes|nds|nns)day$/ => :wednesday,
-      /^wed$/ => :wednesday,
-      /^th(urs|ers)day$/ => :thursday,
-      /^thu(rs)?$/ => :thursday,
-      /^fr[iy](day)?$/ => :friday,
-      /^sat(t?[ue]rday)?$/ => :saturday,
-      /^su[nm](day)?$/ => :sunday
-    }.each do |item, symbol|
-      return Chronic::RepeaterDayName.new(symbol) if item =~ token.word
-    end
-    return nil
-  end
-
-  def self.scan_for_day_portions(token)
-    {
-      /^ams?$/ => :am,
-      /^pms?$/ => :pm,
-      /^mornings?$/ => :morning,
-      /^afternoons?$/ => :afternoon,
-      /^evenings?$/ => :evening,
-      /^(night|nite)s?$/ => :night
-    }.each do |item, symbol|
-      return Chronic::RepeaterDayPortion.new(symbol) if item =~ token.word
-    end
-    return nil
-  end
-
-  def self.scan_for_times(token)
-    if token.word =~ /^\d{1,2}(:?\d{2})?([\.:]?\d{2})?$/
-      return Chronic::RepeaterTime.new(token.word)
-    end
-    return nil
-  end
-
-  def self.scan_for_units(token)
-    {
-      /^years?$/ => :year,
-      /^seasons?$/ => :season,
-      /^months?$/ => :month,
-      /^fortnights?$/ => :fortnight,
-      /^weeks?$/ => :week,
-      /^weekends?$/ => :weekend,
-      /^(week|business)days?$/ => :weekday,
-      /^days?$/ => :day,
-      /^hours?$/ => :hour,
-      /^minutes?$/ => :minute,
-      /^seconds?$/ => :second
-    }.each do |item, symbol|
-      if item =~ token.word
-        klass_name = 'Repeater' + symbol.to_s.capitalize
-        klass = Chronic.const_get(klass_name)
-        return klass.new(symbol)
+module Chronic
+  class Repeater < Tag #:nodoc:
+    def self.scan(tokens)
+      # for each token
+      tokens.each_index do |i|
+        if t = self.scan_for_season_names(tokens[i]) then tokens[i].tag(t); next end
+        if t = self.scan_for_month_names(tokens[i]) then tokens[i].tag(t); next end
+        if t = self.scan_for_day_names(tokens[i]) then tokens[i].tag(t); next end
+        if t = self.scan_for_day_portions(tokens[i]) then tokens[i].tag(t); next end
+        if t = self.scan_for_times(tokens[i]) then tokens[i].tag(t); next end
+        if t = self.scan_for_units(tokens[i]) then tokens[i].tag(t); next end
       end
+      tokens
     end
-    return nil
-  end
 
-  def <=>(other)
-    width <=> other.width
-  end
+    def self.scan_for_season_names(token)
+      scan_for token, RepeaterSeasonName,
+      {
+        /^springs?$/ => :spring,
+        /^summers?$/ => :summer,
+        /^(autumn)|(fall)s?$/ => :autumn,
+        /^winters?$/ => :winter
+      }
+    end
 
-  # returns the width (in seconds or months) of this repeatable.
-  def width
-    raise("Repeatable#width must be overridden in subclasses")
-  end
+    def self.scan_for_month_names(token)
+      scan_for token, RepeaterMonthName,
+      {
+        /^jan\.?(uary)?$/ => :january,
+        /^feb\.?(ruary)?$/ => :february,
+        /^mar\.?(ch)?$/ => :march,
+        /^apr\.?(il)?$/ => :april,
+        /^may$/ => :may,
+        /^jun\.?e?$/ => :june,
+        /^jul\.?y?$/ => :july,
+        /^aug\.?(ust)?$/ => :august,
+        /^sep\.?(t\.?|tember)?$/ => :september,
+        /^oct\.?(ober)?$/ => :october,
+        /^nov\.?(ember)?$/ => :november,
+        /^dec\.?(ember)?$/ => :december
+      }
+    end
 
-  # returns the next occurance of this repeatable.
-  def next(pointer)
-    !@now.nil? || raise("Start point must be set before calling #next")
-    [:future, :none, :past].include?(pointer) || raise("First argument 'pointer' must be one of :past or :future")
-    #raise("Repeatable#next must be overridden in subclasses")
-  end
+    def self.scan_for_day_names(token)
+      scan_for token, RepeaterDayName,
+      {
+        /^m[ou]n(day)?$/ => :monday,
+        /^t(ue|eu|oo|u|)s(day)?$/ => :tuesday,
+        /^tue$/ => :tuesday,
+        /^we(dnes|nds|nns)day$/ => :wednesday,
+        /^wed$/ => :wednesday,
+        /^th(urs|ers)day$/ => :thursday,
+        /^thu(rs)?$/ => :thursday,
+        /^fr[iy](day)?$/ => :friday,
+        /^sat(t?[ue]rday)?$/ => :saturday,
+        /^su[nm](day)?$/ => :sunday
+      }
+    end
 
-  def this(pointer)
-    !@now.nil? || raise("Start point must be set before calling #this")
-    [:future, :past, :none].include?(pointer) || raise("First argument 'pointer' must be one of :past, :future, :none")
-  end
+    def self.scan_for_day_portions(token)
+      scan_for token, RepeaterDayPortion,
+      {
+        /^ams?$/ => :am,
+        /^pms?$/ => :pm,
+        /^mornings?$/ => :morning,
+        /^afternoons?$/ => :afternoon,
+        /^evenings?$/ => :evening,
+        /^(night|nite)s?$/ => :night
+      }
+    end
 
-  def to_s
-    'repeater'
+    def self.scan_for_times(token)
+      if token.word =~ /^\d{1,2}(:?\d{2})?([\.:]?\d{2})?$/
+        return Chronic::RepeaterTime.new(token.word)
+      end
+      return nil
+    end
+
+    def self.scan_for_units(token)
+      {
+        /^years?$/ => :year,
+        /^seasons?$/ => :season,
+        /^months?$/ => :month,
+        /^fortnights?$/ => :fortnight,
+        /^weeks?$/ => :week,
+        /^weekends?$/ => :weekend,
+        /^(week|business)days?$/ => :weekday,
+        /^days?$/ => :day,
+        /^hours?$/ => :hour,
+        /^minutes?$/ => :minute,
+        /^seconds?$/ => :second
+      }.each do |item, symbol|
+        if item =~ token.word
+          klass_name = 'Repeater' + symbol.to_s.capitalize
+          klass = Chronic.const_get(klass_name)
+          return klass.new(symbol)
+        end
+      end
+      return nil
+    end
+
+    def <=>(other)
+      width <=> other.width
+    end
+
+    # returns the width (in seconds or months) of this repeatable.
+    def width
+      raise("Repeatable#width must be overridden in subclasses")
+    end
+
+    # returns the next occurance of this repeatable.
+    def next(pointer)
+      !@now.nil? || raise("Start point must be set before calling #next")
+      [:future, :none, :past].include?(pointer) || raise("First argument 'pointer' must be one of :past or :future")
+      #raise("Repeatable#next must be overridden in subclasses")
+    end
+
+    def this(pointer)
+      !@now.nil? || raise("Start point must be set before calling #this")
+      [:future, :past, :none].include?(pointer) || raise("First argument 'pointer' must be one of :past, :future, :none")
+    end
+
+    def to_s
+      'repeater'
+    end
   end
 end
