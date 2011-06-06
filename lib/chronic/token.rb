147f92e733ea0fcaaed13993d23d27b777fa6026
Lee Jarvis <lee@jarvis.co>
2011-06-06 17:41:51 +0100

    added basic docs for Token

--- lib/chronic/token.rb
+++ lib/chronic/token.rb
@@ -1,6 +1,11 @@
 module Chronic
-  class Token #:nodoc:
-    attr_accessor :word, :tags
+  class Token
+
+    # @return [String] The word this Token represents
+    attr_accessor :word
+
+    # @return [Array] A list of tag associated with this Token
+    attr_accessor :tags
 
     def initialize(word)
       @word = word
@@ -8,21 +13,26 @@ module Chronic
     end
 
     # Tag this token with the specified tag
+    #
+    # @param [Tag] new_tag A instance of {Tag} or one of its subclasses
     def tag(new_tag)
       @tags << new_tag
     end
 
     # Remove all tags of the given class
+    #
+    # @param [Class] The tag class to remove
     def untag(tag_class)
       @tags.delete_if { |m| m.kind_of? tag_class }
     end
 
-    # Return true if this token has any tags
+    # @return [Boolean] true if this token has any tags
     def tagged?
       @tags.size > 0
     end
 
-    # Return the Tag that matches the given class
+    # @param [Class] tag_class The tag class to search for
+    # @return [Tag] The first Tag that matches the given class
     def get_tag(tag_class)
       @tags.find { |m| m.kind_of? tag_class }
     end
