0f60ea06dd30dadcff2a52b9d2fa5ec20a520d34
Lee Jarvis <lee@jarvis.co>
2011-06-06 17:47:28 +0100

    added TimeZone docs

--- lib/chronic/time_zone.rb
+++ lib/chronic/time_zone.rb
@@ -1,11 +1,20 @@
 module Chronic
-  class TimeZone < Tag #:nodoc:
+  class TimeZone < Tag
+
+    # Scan an Array of {Token}s and apply any necessary TimeZone tags to
+    # each token
+    #
+    # @param [Array<Token>] tokens Array of tokens to scan
+    # @param [Hash] options Options specified in {Chronic.parse}
+    # @return [Array] list of tokens
     def self.scan(tokens, options)
       tokens.each_index do |i|
         if t = scan_for_all(tokens[i]) then tokens[i].tag(t); next end
       end
     end
 
+    # @param [Token] token
+    # @return [TimeZone, nil]
     def self.scan_for_all(token)
       scan_for token, self,
       {
