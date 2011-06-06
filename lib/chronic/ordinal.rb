9daa6dca24d156e028a33879d5654ee91564ddab
Lee Jarvis <lee@jarvis.co>
2011-06-06 17:44:32 +0100

    added docs for Ordinal

--- lib/chronic/ordinal.rb
+++ lib/chronic/ordinal.rb
@@ -1,5 +1,12 @@
 module Chronic
-  class Ordinal < Tag #:nodoc:
+  class Ordinal < Tag
+
+    # Scan an Array of {Token}s and apply any necessary Ordinal tags to
+    # each token
+    #
+    # @param [Array<Token>] tokens Array of tokens to scan
+    # @param [Hash] options Options specified in {Chronic.parse}
+    # @return [Array] list of tokens
     def self.scan(tokens, options)
       tokens.each_index do |i|
         if t = scan_for_ordinals(tokens[i]) then tokens[i].tag(t) end
@@ -7,10 +14,14 @@ module Chronic
       end
     end
 
+    # @param [Token] token
+    # @return [Ordinal, nil]
     def self.scan_for_ordinals(token)
       Ordinal.new($1.to_i) if token.word =~ /^(\d*)(st|nd|rd|th)$/
     end
 
+    # @param [Token] token
+    # @return [OrdinalDay, nil]
     def self.scan_for_days(token)
       if token.word =~ /^(\d*)(st|nd|rd|th)$/
         unless $1.to_i > 31 || $1.to_i < 1
