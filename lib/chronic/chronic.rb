aad9048bec00efabb60c1dfa5f2f2ff001b9a599
Lee Jarvis <lee@jarvis.co>
2011-06-06 17:07:15 +0100

    use markdown bullet list to improve parse method docs

--- lib/chronic/chronic.rb
+++ lib/chronic/chronic.rb
@@ -17,41 +17,42 @@ module Chronic
     # or time can be found, +nil+ will be returned.
     #
     # @option opts [Symbol] :context (:future)
-    #   If your string represents a birthday, you can set `:context` to
-    #   `:past` and if an ambiguous string is given, it will assume it is
-    #   in the past. Specify `:future` or omit to set a future context.
+    #   * If your string represents a birthday, you can set `:context` to
+    #     `:past` and if an ambiguous string is given, it will assume it is
+    #     in the past. Specify `:future` or omit to set a future context.
     #
     # @option opts [Object] :now (Time.now)
-    #   By setting `:now` to a Time, all computations will be based off of
-    #   that time instead of `Time.now`. If set to nil, Chronic will use
-    #   `Time.now`.
+    #   * By setting `:now` to a Time, all computations will be based off of
+    #     that time instead of `Time.now`. If set to nil, Chronic will use
+    #     `Time.now`.
     #
     # @option opts [Boolean] :guess (true)
-    #   By default, the parser will guess a single point in time for the given
-    #   date or time. If you'd rather have the entire time span returned,
-    #   set `:guess` to `false` and a {Chronic::Span} will be returned.
+    #   * By default, the parser will guess a single point in time for the
+    #     given date or time. If you'd rather have the entire time span
+    #     returned, set `:guess` to `false` and a {Chronic::Span} will
+    #     be returned.
     #
     # @option opts [Integer] :ambiguous_time_range (6)
-    #   If an Integer is given, ambiguous times (like 5:00) will be
-    #   assumed to be within the range of that time in the AM to that time
-    #   in the PM. For example, if you set it to <tt>7</tt>, then the parser
-    #   will look for the time between 7am and 7pm. In the case of 5:00, it
-    #   would assume that means 5:00pm. If <tt>:none</tt> is given, no
-    #   assumption will be made, and the first matching instance of that
-    #   time will be used.
+    #   * If an Integer is given, ambiguous times (like 5:00) will be
+    #     assumed to be within the range of that time in the AM to that time
+    #     in the PM. For example, if you set it to <tt>7</tt>, then the parser
+    #     will look for the time between 7am and 7pm. In the case of 5:00, it
+    #     would assume that means 5:00pm. If <tt>:none</tt> is given, no
+    #     assumption will be made, and the first matching instance of that
+    #     time will be used.
     #
     # @option opts [Array] :endian_precedence ([:middle, :little])
-    #   By default, Chronic will parse "03/04/2011" as the fourth day
-    #   of the third month. Alternatively you can tell Chronic to parse
-    #   this as the third day of the fourth month by altering the
-    #   `:endian_precedence` to `[:little, :middle]`.
+    #   * By default, Chronic will parse "03/04/2011" as the fourth day
+    #     of the third month. Alternatively you can tell Chronic to parse
+    #     this as the third day of the fourth month by altering the
+    #     `:endian_precedence` to `[:little, :middle]`.
     #
     # @option opts [Integer] :ambiguous_year_future_bias (50)
-    #   When parsing two digit years (ie 79) unlike Rubys Time class, Chronic
-    #   will attempt to assume the full year using this figure. Chronic will
-    #   look x amount of years into the future and past. If the two digit
-    #   year is `now + x years` it's assumed to be the future, `now - x
-    #   years` is assumed to be the past.
+    #   * When parsing two digit years (ie 79) unlike Rubys Time class,
+    #     Chronic will attempt to assume the full year using this figure.
+    #     Chronic will look x amount of years into the future and past. If
+    #     the two digit year is `now + x years` it's assumed to be the
+    #     future, `now - x years` is assumed to be the past.
     #
     # @return [Time, Chronic::Span, nil]
     def parse(text, opts={})
