2fb45f181887176cc821b450c2532569334f5f6d
Lee Jarvis <lee@jarvis.co>
2011-06-07 14:41:43 +0100

    added basic api docs to Chronic::Tag

--- lib/chronic/tag.rb
+++ lib/chronic/tag.rb
@@ -1,13 +1,17 @@
 module Chronic
   # Tokens are tagged with subclassed instances of this class when
   # they match specific criteria
-  class Tag #:nodoc:
+  class Tag
+
+    # @return [Symbol]
     attr_accessor :type
 
+    # @param [Symbol] type
     def initialize(type)
       @type = type
     end
 
+    # @param [Time] s Set the start timestamp for this Tag
     def start=(s)
       @now = s
     end
@@ -15,6 +19,11 @@ module Chronic
     class << self
       private
 
+      # @param [Token] token
+      # @param [Class] klass The class instance to create
+      # @param [Regexp, Hash] items
+      # @return [Object, nil] either a new instance of `klass` or `nil` if
+      #   no match is found
       def scan_for(token, klass, items={})
         case items
         when Regexp
