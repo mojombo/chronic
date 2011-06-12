3720617c012963ec9ab3290bcf5a55325b15fd89
nashby <younash@gmail.com>
2011-06-12 14:52:17 +0300

    ruby 1.8 tests compatibility

--- test/test_parsing.rb
+++ test/test_parsing.rb
@@ -234,14 +234,14 @@ class TestParsing < Test::Unit::TestCase
 
     # old dates
 
-    time = parse_now("may 40")
-    assert_equal Time.local(2040, 5, 16, 12, 0, 0), time
+    time = parse_now("may 32")
+    assert_equal Time.local(2032, 5, 16, 12, 0, 0), time
 
-    time = parse_now("may 27 40")
-    assert_equal Time.local(2040, 5, 27, 12, 0, 0), time
+    time = parse_now("may 27 32")
+    assert_equal Time.local(2032, 5, 27, 12, 0, 0), time
 
-    time = parse_now("1800-08-20")
-    assert_equal Time.local(1800, 8, 20, 12, 0, 0), time
+    time = parse_now("1902-08-20")
+    assert_equal Time.local(1902, 8, 20, 12, 0, 0), time
   end
 
   def test_parse_two_digit_years
@@ -251,8 +251,8 @@ class TestParsing < Test::Unit::TestCase
     time = parse_now("may 1st 01")
     assert_equal Time.local(2001, 5, 1, 12), time
 
-    time = parse_now("may 79", :ambiguous_year_future_bias => 10)
-    assert_equal Time.local(2079, 5, 16, 12), time
+    time = parse_now("may 33", :ambiguous_year_future_bias => 10)
+    assert_equal Time.local(2033, 5, 16, 12), time
   end
 
   def test_parse_guess_r
