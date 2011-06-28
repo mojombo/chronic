dcb77ad632f21dae5ae34292ffa35c644799b1e0
Lee Jarvis <lee@jarvis.co>
2011-06-28 21:03:50 +0100

    remove symbol_to_number private method and use index instead

--- lib/chronic/repeaters/repeater_month_name.rb
+++ lib/chronic/repeaters/repeater_month_name.rb
@@ -24,25 +24,24 @@ module Chronic
       super
 
       if !@current_month_begin
-        target_month = symbol_to_number(@type)
         case pointer
         when :future
-          if @now.month < target_month
-            @current_month_begin = Time.construct(@now.year, target_month)
-          elsif @now.month > target_month
-            @current_month_begin = Time.construct(@now.year + 1, target_month)
+          if @now.month < index
+            @current_month_begin = Time.construct(@now.year, index)
+          elsif @now.month > index
+            @current_month_begin = Time.construct(@now.year + 1, index)
           end
         when :none
-          if @now.month <= target_month
-            @current_month_begin = Time.construct(@now.year, target_month)
-          elsif @now.month > target_month
-            @current_month_begin = Time.construct(@now.year + 1, target_month)
+          if @now.month <= index
+            @current_month_begin = Time.construct(@now.year, index)
+          elsif @now.month > index
+            @current_month_begin = Time.construct(@now.year + 1, index)
           end
         when :past
-          if @now.month >= target_month
-            @current_month_begin = Time.construct(@now.year, target_month)
-          elsif @now.month < target_month
-            @current_month_begin = Time.construct(@now.year - 1, target_month)
+          if @now.month >= index
+            @current_month_begin = Time.construct(@now.year, index)
+          elsif @now.month < index
+            @current_month_begin = Time.construct(@now.year - 1, index)
           end
         end
         @current_month_begin || raise("Current month should be set by now")
@@ -85,17 +84,11 @@ module Chronic
     end
 
     def index
-      symbol_to_number(@type)
+      @index ||= MONTHS[@type]
     end
 
     def to_s
       super << '-monthname-' << @type.to_s
-    end
-
-    private
-
-    def symbol_to_number(sym)
-      MONTHS[sym] || raise("Invalid symbol specified")
     end
   end
 end
