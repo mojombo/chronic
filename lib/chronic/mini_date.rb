5d8dd483b6321cc818f8360d28d5af48378b5efd
Lee Jarvis <lee@jarvis.co>
2011-07-03 21:33:43 +0100

    use && over and

--- lib/chronic/mini_date.rb
+++ lib/chronic/mini_date.rb
@@ -12,11 +12,13 @@ module Chronic
     end
 
     def is_between?(md_start, md_end)
-      return false if (@month == md_start.month && @month == md_end.month &&
-                      (@day < md_start.day || @day > md_end.day))
-      return true if (@month == md_start.month and @day >= md_start.day) ||
-                     (@month == md_end.month and @day <= md_end.day)
+      return false if (@month == md_start.month && @month == md_end.month) &&
+                      (@day < md_start.day || @day > md_end.day)
+      return true if (@month == md_start.month && @day >= md_start.day) ||
+                     (@month == md_end.month && @day <= md_end.day)
+
       i = (md_start.month % 12) + 1
+
       until i == md_end.month
         return true if @month == i
         i = (i % 12) + 1
