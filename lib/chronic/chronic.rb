7c5f962f5af6f4c1cdd9c585f8fc8d7abe494180
Lee Jarvis <lee@jarvis.co>
2012-05-31 17:03:35 +0100

    modify an? gsub regexp to simplify

--- lib/chronic/chronic.rb
+++ lib/chronic/chronic.rb
@@ -123,7 +123,7 @@ module Chronic
       text.gsub!(/\b\d+:?\d*[ap]\b/,'\0m')
       text.gsub!(/(\d)([ap]m|oclock)\b/, '\1 \2')
       text.gsub!(/\b(hence|after|from)\b/, 'future')
-      text.gsub!(/^\s?[aA][nN]? /, '1 ')
+      text.gsub!(/^\s?an? /i, '1 ')
       text
     end
 
