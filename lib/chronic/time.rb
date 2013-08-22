dbf2e0cffc2c41c3933d9a500912476785583430
Dāvis <davispuh@gmail.com>
2013-08-22 16:35:24 +0300

    Add offset support for Chronic::construct and improve Date, Time classes

--- lib/chronic/time.rb
+++ lib/chronic/time.rb
@@ -1,5 +1,9 @@
 module Chronic
   class Time
+    HOUR_SECONDS = 3600 # 60 * 60
+    MINUTE_SECONDS = 60
+    SECOND_SECONDS = 1 # haha, awesome
+    SUBSECOND_SECONDS = 0.001
 
     # Checks if given number could be hour
     def self.could_be_hour?(hour)
@@ -19,6 +23,17 @@ module Chronic
     # Checks if given number could be subsecond
     def self.could_be_subsecond?(subsecond)
       subsecond >= 0 && subsecond <= 999999
+    end
+
+    # normalize offset in seconds to offset as string +mm:ss or -mm:ss
+    def self.normalize_offset(offset)
+      return offset if offset.is_a?(String)
+      offset = Chronic.time_class.now.to_time.utc_offset unless offset # get current system's UTC offset if offset is nil
+      sign = '+'
+      sign = '-' if offset < 0
+      hours = (offset.abs / 3600).to_i.to_s.rjust(2,'0')
+      minutes = (offset.abs % 3600).to_s.rjust(2,'0')
+      sign + hours + minutes
     end
 
   end
