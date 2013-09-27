1853dab00362c1dca51dea69248b0aa13d7aeb91
Jean-Paul Bonnetouche <goodtouch@gmail.com>
2013-09-27 19:25:34 +0200

    Add an option to set the first day of the week

--- lib/chronic/repeaters/repeater_week.rb
+++ lib/chronic/repeaters/repeater_week.rb
@@ -4,6 +4,7 @@ module Chronic
 
     def initialize(type, options = {})
       super
+      @repeater_day_name = options[:week_start] || :sunday
       @current_week_start = nil
     end
 
@@ -13,16 +14,16 @@ module Chronic
       unless @current_week_start
         case pointer
         when :future
-          sunday_repeater = RepeaterDayName.new(:sunday)
-          sunday_repeater.start = @now
-          next_sunday_span = sunday_repeater.next(:future)
-          @current_week_start = next_sunday_span.begin
+          first_week_day_repeater = RepeaterDayName.new(@repeater_day_name)
+          first_week_day_repeater.start = @now
+          next_span = first_week_day_repeater.next(:future)
+          @current_week_start = next_span.begin
         when :past
-          sunday_repeater = RepeaterDayName.new(:sunday)
-          sunday_repeater.start = (@now + RepeaterDay::DAY_SECONDS)
-          sunday_repeater.next(:past)
-          last_sunday_span = sunday_repeater.next(:past)
-          @current_week_start = last_sunday_span.begin
+          first_week_day_repeater = RepeaterDayName.new(@repeater_day_name)
+          first_week_day_repeater.start = (@now + RepeaterDay::DAY_SECONDS)
+          first_week_day_repeater.next(:past)
+          last_span = first_week_day_repeater.next(:past)
+          @current_week_start = last_span.begin
         end
       else
         direction = pointer == :future ? 1 : -1
@@ -38,23 +39,23 @@ module Chronic
       case pointer
       when :future
         this_week_start = Chronic.time_class.local(@now.year, @now.month, @now.day, @now.hour) + RepeaterHour::HOUR_SECONDS
-        sunday_repeater = RepeaterDayName.new(:sunday)
-        sunday_repeater.start = @now
-        this_sunday_span = sunday_repeater.this(:future)
-        this_week_end = this_sunday_span.begin
+        first_week_day_repeater = RepeaterDayName.new(@repeater_day_name)
+        first_week_day_repeater.start = @now
+        this_span = first_week_day_repeater.this(:future)
+        this_week_end = this_span.begin
         Span.new(this_week_start, this_week_end)
       when :past
         this_week_end = Chronic.time_class.local(@now.year, @now.month, @now.day, @now.hour)
-        sunday_repeater = RepeaterDayName.new(:sunday)
-        sunday_repeater.start = @now
-        last_sunday_span = sunday_repeater.next(:past)
-        this_week_start = last_sunday_span.begin
+        first_week_day_repeater = RepeaterDayName.new(@repeater_day_name)
+        first_week_day_repeater.start = @now
+        last_span = first_week_day_repeater.next(:past)
+        this_week_start = last_span.begin
         Span.new(this_week_start, this_week_end)
       when :none
-        sunday_repeater = RepeaterDayName.new(:sunday)
-        sunday_repeater.start = @now
-        last_sunday_span = sunday_repeater.next(:past)
-        this_week_start = last_sunday_span.begin
+        first_week_day_repeater = RepeaterDayName.new(@repeater_day_name)
+        first_week_day_repeater.start = @now
+        last_span = first_week_day_repeater.next(:past)
+        this_week_start = last_span.begin
         Span.new(this_week_start, this_week_start + WEEK_SECONDS)
       end
     end
